14987dca4031c87ba39b2ae4b56852e403d5c516
sl1pknot@abv.bg <sl1pknot@abv.bg>
2025-07-13 17:02:30 +0300

    Initial commit: Add main.c

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <errno.h>
+
+int main() {
+    
+    while(1) {
+        char line[1024] = {0};
+        char *args[64] = {0};
+        int i = 0;
+        
+        printf("lzsh> ");
+        fgets(line, sizeof(line), stdin);
+        line[strcspn(line, "\n")] = '\0'; //removing the new line at the back
+
+        char *linePtr = strtok(line, " "); //tokenizing everything between space in the input
+
+        while(linePtr != 0) {
+            args[i++] = linePtr; //adding into the array the token and every other 
+            linePtr = strtok(NULL, " "); //continuing tokenizing after the first one where it stopped
+        }    
+        args[i] = NULL;
+
+        // chdir("/working");
+
+        // char *dir = "/mnt/sdb1/python projects/lzshell/working";
+        // int ret_dir = chdir(dir);
+
+        // if (args[0] == "cd") {
+        if (strcmp(args[0], "cd") == 0) {
+            // int ret_dir = chdir(dir);
+            int ret_dir = chdir(args[1]);
+            if (ret_dir != 0) {
+                printf("Value of errno: %d\n", errno);
+            }
+            else {
+                printf("success\n");
+            }
+            printf("%s\n", args[0]);
+            printf("%s\n", getcwd(*args, 64));
+        }
+        // printf("%s\n", args[0]);
+        // printf("%s\n", getcwd(*args, 64));
+        else {
+            pid_t proc_fork = fork();
+            if (proc_fork == 0) {
+                execvp(args[0], args);
+                perror("exec failed");
+                exit(1);
+            }
+            else if (proc_fork > 0) {
+                wait(NULL);
+            }
+            else {
+                perror("fork failed");
+            }
+        }
+        memset(args, 0, sizeof(args));
+    }
+    return 0;
+}
